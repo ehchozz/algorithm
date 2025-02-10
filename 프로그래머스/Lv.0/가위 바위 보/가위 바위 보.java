a4dbdb809bf3bf4ff75123c2583dcbec93db28a8
Eunho Cho <ehcho1231@gmail.com>
2025-02-10 19:42:02 +0900

    Create 가위 바위 보.java

---
+++ 프로그래머스/Lv.0/가위 바위 보/가위 바위 보.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String solution(String rsp) {
+        String answer = "";
+        
+        for (int i=0; i<rsp.length(); i++) {
+            String s = rsp.split("")[i];
+            if (s.equals("0")) answer += "5";
+            if (s.equals("2")) answer += "0";
+            if (s.equals("5")) answer += "2";
+        }
+        
+        return answer;
+    }
+}
